04b57dc25d7b465482d9684f6588b86fa065c57d
Joshua Lee <josh.jk.lee@gmail.com>
2024-01-09 10:05:27 -0700

    images added and sorted

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import '.App/css';
+import Header from './components/Header';
+
+const HomePage = () => {
+    return (
+        
+        <Header />
+
+
+    )
+};
+
+export default HomePage;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import './header.css';
+
+import React from 'react';
+
+const header = () => {
+    return (
+        <header className="header">
+      <div className="header__content">
+        {/* Your header content goes here */}
+        <p>Welcome to my Portfolio</p>
+      </div>
+    </header>
+    )
+}
+
+export default header;
