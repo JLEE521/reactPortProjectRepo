48543f34c0cf3b3138f0e8eb5e38c87d87f68b20
Joshua Lee <josh.jk.lee@gmail.com>
2024-01-19 20:17:03 -0700

    homepage made paths not working yet

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,11 +2,14 @@ import { Link } from 'react-router-dom';
 import { Container, Row, Col, Card, CardImg, CardBody, CardTitle } from 'reactstrap';
 import React from 'react';
 import paper6 from '../src/images/paperPeople/paper_6.jpg';
+import swampFrog from '../src/images/swamp/swampFrog.JPG';
+import charcoalWings from '../src/images/charcoal/charcoalWings.jpg';
+import sbLibrary from '../src/images/sketchbook/sb_library.jpg';
 
 const ImageCard = ({ imageUrl, title, to }) => (
     <Col md={4} className="mb-4">
-      <Link to={to}>
-        <Card>
+      <Link to={to} className="text-decoration-none" style={{ cursor: 'pointer' }}>
+        <Card style={{ maxWidth: '600px', margin: '50px' }}>
           <CardImg top width="100%" src={imageUrl} alt={title} />
           <CardBody>
             <CardTitle>{title}</CardTitle>
@@ -20,10 +23,10 @@ const ImageCard = ({ imageUrl, title, to }) => (
 
 const HomePage = () => {
     const imageData = [
-        { imageUrl: paper6, title: 'Paper People', to: '/page1' },
-        { imageUrl: '', title: 'Swamp', to: '/page2' },
-        { imageUrl: '', title: 'Charcaol Drawings', to: '/page2' },
-        { imageUrl: '', title: 'Sketchbook Drawings', to: '/page2' },
+        { imageUrl: paper6, title: '', to: '/paperPeople' },
+        { imageUrl: swampFrog, title: '', to: '/page2' },
+        { imageUrl: charcoalWings, title: '', to: '/page2' },
+        { imageUrl: sbLibrary, title: '', to: '/page2' },
       ];
 
     return (
